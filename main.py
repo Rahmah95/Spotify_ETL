6b513c45b3dc872f1f689df7ac41befdd499315c
Blushie <32341540+Rahmah95@users.noreply.github.com>
2023-10-28 14:36:21 +0200

    improve readability and modularity

--- main.py
+++ main.py
@@ -4,77 +4,162 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import json
 import pandas as pd
 
-client_id = API_config.client_id
-client_secret = API_config.client_secret
-client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+# Set the Spotify API client credentials
+CLIENT_ID = API_config.client_id
+CLIENT_SECRET = API_config.client_secret
 
-# Line below is for Top 50 Global songs
-playlist_link = "https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=3ba7f1262cdb41aa"
-playlist_URI = playlist_link.split("/")[-1].split("?")[0]
-data = sp.playlist_items(playlist_URI)
 
-# dump the data to a JSON file
-json_file_path = "spotify_data.json"
+# Function to fetch playlist data from Spotify
+def fetch_playlist_data(playlist_link):
+    """ To fetch playlist data from Spotify """
+    # Initialize a Spotify client
+    client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    # Extract the playlist URI from the provided link
+    playlist_URI = playlist_link.split("/")[-1].split("?")[0]
+    # Use the Spotify client to fetch data for the playlist
+    data = sp.playlist_items(playlist_URI)
+    return data
 
-with open(json_file_path, 'w', encoding='utf-8') as json_file:
-    json.dump(data, json_file, ensure_ascii=False, indent=4)
 
-# print(f"Data has been saved to {json_file_path}")
+# Function to save data to a JSON file
+def save_data_to_json(data, json_file_path):
+    """ To save raw data to a JSON file """
+    with open(json_file_path, 'w', encoding='utf-8') as json_file:
+        # Write the playlist data to the JSON file with formatting
+        json.dump(data, json_file, ensure_ascii=False, indent=4)
 
-# Declare variables to store extracted data
-album_list = []   # List to store album data for all items
-artist_list = []  # List to store artist data for all items
-song_list = []    # List to store song data for all items
 
-# Extract album, artist, and song data
-for item in data['items']:
-    track = item.get("track", {})
-    albums = track.get("album", {})
-    artists = track.get("artists", [])
-
-    # Extract album data
+# Function to extract album data
+def extract_album(album):
+    """ To extract album data """
     album_data = {
-        "album_id": albums['id'],
-        "album_name": albums['name'],
-        "release_date": albums['release_date'],
-        "total_tracks": albums['total_tracks'],
-        "external_urls": albums['external_urls']['spotify']
+        "album_id": album['id'],
+        "album_name": album['name'],
+        "release_date": album['release_date'],
+        "total_tracks": album['total_tracks'],
+        "external_urls": album['external_urls']['spotify']
     }
-    album_list.append(album_data)
+    return album_data
 
-    # Extract artist data
-    for artist in artists:
-        artist_data = {
-            'artist_id': artist['id'],
-            'artist_name': artist['name'],
-            'external_url': artist['external_urls']['spotify']
-        }
-        artist_list.append(artist_data)
 
-    # Extract song data
-    song_data = {
-        'song_id': track['id'],
-        'song_name': track['name'],
-        'song_duration_ms': track['duration_ms'],
-        'song_url': track['external_urls']['spotify'],
-        'song_popularity': track['popularity'],
-        'added_at': item['added_at'],
-        'album_id': albums['id'],
-        'artist_id': artist_data['artist_id'],
-        # 'artist_id': [artist['id'] for artist in artists]
-
+# Function to extract artist data
+def extract_artist(artist):
+    """ To extract artist data """
+    artist_data = {
+        'artist_id': artist['id'],
+        'artist_name': artist['name'],
+        'external_url': artist['external_urls']['spotify']
     }
-    song_list.append(song_data)
+    return artist_data
 
-# Create DataFrames (df) from the lists
-album_df = pd.DataFrame(album_list)
-artist_df = pd.DataFrame(artist_list)
-song_df = pd.DataFrame(song_list)
 
-# transform release_date and added_date to datetime
-album_df['release_date'] = pd.to_datetime(album_df['release_date'])
-album_df.info()  # verify release_date is datetime
-song_df['added_at'] = pd.to_datetime(song_df['added_at'])
-song_df.info()  # verify added_date is datetime
+# Function to extract song data
+def extract_song_data(data):
+    """ To extract song data """
+    song_data_list = []
+    for item in data['items']:
+        track = item.get("track", {})
+        albums = track.get("album", {})
+        artists = track.get("artists", [])
 
+        for artist in artists:
+            artist_data = extract_artist(artist)
+
+            song_data = {
+                'song_id': track['id'],
+                'song_name': track['name'],
+                'song_duration_ms': track['duration_ms'],
+                'song_url': track['external_urls']['spotify'],
+                'song_popularity': track['popularity'],
+                'added_at': item['added_at'],
+                'album_id': albums['id'],
+                'artist_id': artist_data['artist_id']
+            }
+            song_data_list.append(song_data)
+
+    return song_data_list
+
+
+# Function to validate and clean dataframes
+def validate_dataframes(song_df, album_df, artist_df):
+    """ To validate and clean dataframes """
+    # Remove duplicate rows based on 'song_id', 'album_id', and 'artist_id' columns
+    song_df.drop_duplicates(subset='song_id', inplace=True)
+    album_df.drop_duplicates(subset='album_id', inplace=True)
+    artist_df.drop_duplicates(subset='artist_id', inplace=True)
+
+    # Remove rows with any null values in the DataFrames
+    song_df.dropna(how='any', inplace=True)
+    album_df.dropna(how='any', inplace=True)
+    artist_df.dropna(how='any', inplace=True)
+
+
+# Function to change date columns to datetime format
+def transform_dateframes(song_df, album_df, artist_df):
+    """ To transfrom date in DataFrames """
+    # Check if 'added_at' column is not already in datetime format
+    if not pd.api.types.is_datetime64_ns_dtype(song_df['added_at']):
+        song_df['added_at'] = pd.to_datetime(song_df['added_at'])
+
+    # Remove the timezone information
+    song_df['added_at'] = song_df['added_at'].dt.tz_localize(None)
+
+    # Check and convert the 'release_date' column in the album_df
+    if not pd.api.types.is_datetime64_ns_dtype(album_df['release_date']):
+        album_df['release_date'] = pd.to_datetime(album_df['release_date'])
+
+    # Remove the timezone information
+    album_df['release_date'] = album_df['release_date'].dt.tz_localize(None)
+
+
+# Function to create and save Excel files for each DataFrame
+def export_dataframes(song_df, album_df, artist_df):
+    """ To  create and save dataframes into Excel files """
+    song_df.to_excel("files/song_data.xlsx", index=False)
+    album_df.to_excel("files/album_data.xlsx", index=False)
+    artist_df.to_excel("files/artist_data.xlsx", index=False)
+
+
+# Main function to fetch and process playlist data
+def main(playlist_link, json_file_path):
+    """ To put all functions to work together """
+    # Fetch data from the Spotify playlist
+    data = fetch_playlist_data(playlist_link)
+    # Save the fetched data to a JSON file
+    save_data_to_json(data, json_file_path)
+
+    # Extract and process song, album, and artist data
+    song_data_list = extract_song_data(data)
+    album_data = [extract_album(item['track']['album']) for item in data['items']]
+    artist_data = [extract_artist(artist) for item in data['items'] for artist in item['track']['artists']]
+
+    # Convert the processed data into Pandas DataFrames
+    song_df = pd.DataFrame(song_data_list)
+    album_df = pd.DataFrame(album_data)
+    artist_df = pd.DataFrame(artist_data)
+
+    # Validate and clean dataframes
+    validate_dataframes(song_df, album_df, artist_df)
+
+    # Transform data
+    transform_dateframes(song_df, album_df, artist_df)
+
+    # Save data to Excel files
+    export_dataframes(song_df, album_df, artist_df)
+
+    return song_df, album_df, artist_df
+
+
+# Check if the script is run as the main program
+if __name__ == '__main__':
+    # Specify the Spotify playlist link and the JSON file path
+    playlist_link = "https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=3ba7f1262cdb41aa"
+    json_file_path = "spotify_data.json"
+
+    # Call the main function with the JSON file path as a parameter
+    song_df = main(playlist_link, json_file_path)[0]
+    album_df = main(playlist_link, json_file_path)[1]
+    artist_df = main(playlist_link, json_file_path)[2]
+    print(song_df)
+    # song_df, album_df, artist_df = main(playlist_link, json_file_path)
